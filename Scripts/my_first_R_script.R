d91d9bce5525b3d59f5f88dd683e03518d93e1dd
raog2491992 <raog2491992@gmail.com>
2021-01-10 20:58:34 -0500

    Initial commit

---
+++ Scripts/my_first_R_script.R
@@ -0,0 +1,17 @@
+mean(mtcars$mpg)
+example <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8), nrow = 4, ncol = 2)
+1+1
+View(example)
+View(example)
+setwd("~/Coursera/R")
+install.packages("ggplot2")
+library(ggplot2)
+installed.packages()
+library()
+old.packages()
+version
+sessionInfo()
+help(package = "ggplot2")
+browseVignettes("ggplot2")
+library(KernSmooth)
+
